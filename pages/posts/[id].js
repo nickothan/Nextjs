574e88eb34a68f1eb5bbfb473d4705d46ad383f1
Nickothan <s1nicolasrg@gmail.com>
2021-12-07 09:27:08 -0500

    Render Markdown

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -11,7 +11,8 @@ export async function getStaticPaths() {
 }
 
 export async function getStaticProps({ params }) {
-  const postData = getPostData(params.id)
+  // Add the "await" keyword like this:
+  const postData = await getPostData(params.id)
   return {
     props: {
       postData
@@ -27,6 +28,8 @@ export default function Post({ postData }) {
       {postData.id}
       <br />
       {postData.date}
+      <br />
+      <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
     </Layout>
   )
 }
